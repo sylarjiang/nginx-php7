1fbe29127db20cc6634eda41745ed47986a20fd0
sylar-office <sylar@office>
2016-10-25 16:07:42 +0800

    删除：      ../Dockerfile 	删除：      ../README.md 	删除：      ../changelogs.md

--- index.php
+++
@@ -1,6 +0,0 @@
-<div style="text-align:center;">
-<strong>GitHub: </strong><a href="https://github.com/sylarjiang/nginx-php7" target="_blank">https://github.com/skiy-dockerfile/nginx-php7</a><br />
-</div>
-
-<?php
-phpinfo();

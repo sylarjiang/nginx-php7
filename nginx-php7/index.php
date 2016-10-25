481eb9d7e4efc9ef2e80541e6a494f956bc721e9
sylar-office <sylar@office>
2016-10-25 16:08:46 +0800

    新文件：    nginx-php7/Dockerfile

---
+++ nginx-php7/index.php
@@ -0,0 +1,6 @@
+<div style="text-align:center;">
+<strong>GitHub: </strong><a href="https://github.com/sylarjiang/nginx-php7" target="_blank">https://github.com/skiy-dockerfile/nginx-php7</a><br />
+</div>
+
+<?php
+phpinfo();
